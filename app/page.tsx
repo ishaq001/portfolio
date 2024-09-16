42b9f3bd2f0f3a865d55fb46af87a11e96afc1c2
ishaq <orakk2211@gmail.com>
2024-09-17 02:04:33 +0500

    add navbar , grid sections, react-icons and framer motion library

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
+import Grid from "@/components/Grid"
 import Hero from "@/components/Hero"
+import { FloatingNav } from "@/components/ui/FloatingNav"
 import Image from "next/image"
+import { FaHome } from "react-icons/fa"
 
 export default function Home() {
   return (
     <main className='relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mr-auto sm:px-10 px-5'>
       <div className='max-w-7xl w-full'>
+        <FloatingNav
+          className='relative'
+          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
+        />
         <Hero />
+        <Grid />
       </div>
     </main>
   )
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from "react"
 import { Spotlight } from "./ui/Spotlight"
+import { TextGenerateEffect } from "./ui/TextGenerateEffect"
+import MagicButton from "./ui/MagicButton"
+import { FaLocationArrow } from "react-icons/fa"
 
 const Hero = () => {
   return (
@@ -18,12 +21,43 @@ const Hero = () => {
         fill='grey'
       />
 
-      <div className='h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center'>
+      <div
+        className='h-screen w-full dark:bg-black-100 bg-white 
+       dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0  flex items-center justify-center'
+      >
         <div
           className='absolute pointer-events-none inset-0 flex 
         items-center justify-center dark:bg-black-100 bg-white
          [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'
         />
+      </div>
+
+      <div className='flex justify-center relative my-20 z-10'>
+        <div className='max-2-[89vw] md:max-w-2xl lg:max-2-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest text-xs text-center max-w-80 text-blue-100'>
+            Muhammad Ishaq
+          </h2>
+          <TextGenerateEffect
+            words='Empowering Innovation Through Code and Creativity.'
+            duration={1}
+            className='text-center text-[40px] md:text-5xl lg:text-6xl'
+          />
+          <p
+            className='text-center md:tracking-wider 
+                mb-4 text-sm md:text-lg lg:text-2xl'
+          >
+            I am Muhammad Ishaq, a passionate Software Engineer specializing in
+            creating dynamic and user-friendly web applications using modern
+            technologies.
+          </p>
+          <a href='#about'>
+            <MagicButton
+              title='Show my work'
+              icon={<FaLocationArrow />}
+              position='right'
+            />
+          </a>
+        </div>
       </div>
     </div>
   )
